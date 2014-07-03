1546514d690f96ddec9a63eb639f18f213b9adc5
kennethjcampbell <kennethjcampbell@gmail.com>
2014-07-02 22:16:08 -0400

    Rev 2

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-	
-
 $(document).ready(function() {
 
     $("#itemName").click(function() {	
@@ -7,14 +5,27 @@ $(document).ready(function() {
     });
 
     $('#searchContainer').on('click','input[name="add"]', function() {
-      $('ul').append('<li>'+$('#itemName').val()+'<button class="delete">Delete</button><button class="complete">Complete</button></li>');
-      $('#itemName').val("");
-      $('.delete').click(function() {
-      	$(this).closest("li").remove();
+      addItem();
+    });
+
+    $('#searchContainer').on('keydown','input[name="add"]', function(event) {
+      if( event.which == 13) {
+       $('ul').append('<li>'+$('#itemName').val()+'<button class="delete">Delete</button><button class="complete">Complete</button></li>');
+       $('#itemName').val("");
+      }
+    });
+
+    $('#center').on('click','.delete',function() {
+        $(this).closest("li").remove();
       });
-      $('.complete').click(function() {
-      	$(this).closest("li").css("text-decoration", "line-through");
-      });
-  });
+
+    $('#center').on('click', '.complete', function() {
+        $(this).closest("li").css("text-decoration", "line-through");
+    });
+
+    function addItem(){
+        $('ul').append('<li>'+$('#itemName').val()+'<button class="delete">Delete</button><button class="complete">Complete</button></li>');
+        $('#itemName').val("");
+      }
 });
 
